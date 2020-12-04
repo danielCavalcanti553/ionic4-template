cad49c7a5783b771d4c6ed94869fdd1941bfb472
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2020-12-04 02:51:41 -0300

    Minha versão

--- src/model/cliente.ts
+++ src/model/cliente.ts
@@ -3,7 +3,7 @@ export class Cliente{
     nome : string;
     email : string;
     telefone : string;
-    teste1: string;
+    teste1:string;
     
     setCliente(obj : any, id : any){
         this.id = id;
